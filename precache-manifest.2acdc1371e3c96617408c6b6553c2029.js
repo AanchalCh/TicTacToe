3c7baa2671b3d0b1bf2721e33a87d5588873d02d
AanchalCh <aanchal.chaurasia99@gmail.com>
2020-07-23 20:17:08 +0530

    Updates

---
+++ precache-manifest.2acdc1371e3c96617408c6b6553c2029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b81d232881e6d9c902f55a14e9ceb6",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "babbc8ce5e6f4536bd42",
+    "url": "/TicTacToe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "bd0bec83d3fd6cef6ad2",
+    "url": "/TicTacToe/static/js/2.8412e8be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.8412e8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babbc8ce5e6f4536bd42",
+    "url": "/TicTacToe/static/js/main.66a16d83.chunk.js"
+  },
+  {
+    "revision": "4984d286a2ebf4f80f5b",
+    "url": "/TicTacToe/static/js/runtime-main.a2c6be0f.js"
+  }
+]);
